ecceb12a3b80e9922ff8d67d63a67c6a9c23c0c3
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-01-06 21:38:49 +0100

    Update basket status in menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,27 +2,43 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CurrencyService } from '../../shared/services/currency.service';
 import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { ToursBookingService } from '../../shared/services/tours-booking.service';
+import { ToursService } from '../../shared/services/tours.service';
+import { SummaryComponent } from '../summary/summary.component';
 
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle, NgClass],
+  imports: [RouterLink, NgIf, SummaryComponent, RouterLinkActive, NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle, NgClass],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
-  currency: CurrencyService;
+  constructor(private currency: CurrencyService, private bookingService: ToursBookingService, toursService: ToursService) {
 
-  constructor(currency: CurrencyService) {
-    this.currency = currency;
   }
 
   isCurrencyActive(currencyCode: string) {
     return this.currency.getCurrencyCode() === currencyCode;
   }
 
+  getCurrencyCode() {
+    return this.currency.getCurrencyCode();
+  }
+
+  setCurrencyCode(currencyCode: string) {
+    this.currency.setCurrencyCode(currencyCode);
+  }
+
+  hasAnyBooking() {
+    return this.bookingService.hasAnyBooking();
+  }
+
+  getNumberOfBookedTours() {
+    return this.bookingService.getNumberOfBookedTours();
+  }
 
 }
