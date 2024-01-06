2423a99741f7b67d98f3b1c47397d66e1101c161
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-01-06 16:47:46 +0100

    Update routes and global features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ToursComponent } from './tours/tours.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SummaryComponent } from './summary/summary.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToursComponent, HttpClientModule, SummaryComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet]
 })
 export class AppComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ToursComponent } from './components/tours/tours.component';
+import { HistoryComponent } from './components/history/history.component';
+import { BasketPageComponent } from './components/basket-page/basket-page.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'basket', pathMatch: 'full' },
+  { path: 'tours', component: ToursComponent },
+  { path: 'basket', component: BasketPageComponent },
+  { path: 'history', component: HistoryComponent },
+];
