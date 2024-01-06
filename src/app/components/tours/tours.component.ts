bd652a0470f304335ab4f816767acfd925b5b1ba
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-01-06 16:50:33 +0100

    Rework tours

--- src/app/components/tours/tours.component.ts
+++ src/app/components/tours/tours.component.ts
@@ -1,129 +1,107 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CurrencyPipe, NgClass, NgFor, NgIf, NgStyle, UpperCasePipe } from '@angular/common';
 import { FormGroup, ReactiveFormsModule, FormControl, Validators, FormsModule } from '@angular/forms';
-import { FilteredToursPipe } from '../shared/filtered-tours.pipe';
+import { FilteredToursPipe } from '../../shared/pipes/filtered-tours.pipe';
 import { TourComponent } from '../tour/tour.component';
 import { BasketComponent } from '../basket/basket.component';
 import { HistoryComponent } from '../history/history.component';
+import { MenuComponent } from '../menu/menu.component';
+import { ToursService } from '../../shared/services/tours.service';
+import { Tour } from '../../shared/interfaces/tour';
+import { ToursFilterService } from '../../shared/services/tours-filter.service';
+import { ToursFilterComponent } from '../tours-filter/tours-filter.component';
 
-
-const TOURS_PATH = "../../assets/tours.json";
 
 @Component({
   selector: 'app-tours',
   standalone: true,
-  imports: [NgFor, BasketComponent, TourComponent, NgIf, NgClass, NgStyle, FilteredToursPipe, UpperCasePipe, CurrencyPipe, FormsModule, ReactiveFormsModule, HistoryComponent],
+  imports: [NgFor, BasketComponent, TourComponent, NgIf, NgClass, NgStyle, FilteredToursPipe, UpperCasePipe, CurrencyPipe, FormsModule, ReactiveFormsModule, HistoryComponent, MenuComponent, ToursFilterComponent],
   templateUrl: './tours.component.html',
   styleUrl: './tours.component.css'
 })
 export class ToursComponent {
 
-  tours: any;
-  currencyCode: string = 'EUR';
-  tourForm: any;
   toursBooking: { [key: string]: number } = {};
 
-  minPriceFilter: any = null;
-  maxPriceFilter: any = null;
-  startDateFilter: any = null;
-  endDateFilter: any = null;
-  ratingsFilter: any = {};
-  destinationFilter: any = {};
-  history: any = [];
 
-  constructor(private http: HttpClient) {
-    this.http.get('assets/tours.json').subscribe(data => {
 
-      this.tours = data;
+  constructor(private toursService: ToursService, public toursFilterService: ToursFilterService) {
 
-      this.tours = this.tours.map((tour: any) => {
-        return tour;
-      });
-
-      this.tours.forEach((tour: any) => {
-        this.toursBooking[tour.id] = 0;
-      });
-    });
-
-    this.tourForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
-      targetCountry: new FormControl('', [Validators.required]),
-      startDate: new FormControl('', [Validators.required]),
-      endDate: new FormControl('', [Validators.required]),
-      price: new FormControl('', [Validators.required, Validators.max(10000)]),
-      maxPeople: new FormControl('', [Validators.required, Validators.max(30)]),
-      description: new FormControl('', [Validators.required, Validators.maxLength(500)]),
-      image: new FormControl('', [Validators.required])
-    });
   }
 
-  reload() { this.tours = this.tours?.map((tour: any) => { return tour }); }
-
-  getAvailableDestinations() {
-
-    let available = this.tours?.map((tour: any) => tour.targetCountry) || [];
-
-    available = available.filter((country: any, index: any) => {
-      return available.indexOf(country) === index;
-    })
-
-    for (let destination of available) {
-      if (!Object.keys(this.destinationFilter).includes(destination)) {
-        this.destinationFilter[destination] = true;
-      }
-    }
-
-    for (let destination of Object.keys(this.destinationFilter)) {
-      if (!available.includes(destination)) {
-        this.destinationFilter[destination] = true;
-      }
-    }
-
-    return available;
+  getTours() {
+    return this.toursService.getTours();
   }
 
-  getAvailableRatings() {
+  // reload() { this.tours = this.tours?.map((tour: any) => { return tour }); }
 
-    let available = this.tours?.map((tour: any) => String(tour.rating)) || [];
+  // getAvailableDestinations() {
 
-    available = available.filter((rating: any, index: any) => {
-      return available.indexOf(rating) === index;
-    })
+  //   let available = this.tours?.map((tour: any) => tour.targetCountry) || [];
 
-    for (let rating of available) {
-      if (!Object.keys(this.ratingsFilter).includes(rating.toString())) {
-        this.ratingsFilter[rating] = true;
-      }
-    }
+  //   available = available.filter((country: any, index: any) => {
+  //     return available.indexOf(country) === index;
+  //   })
 
-    return available;
-  }
+  //   for (let destination of available) {
+  //     if (!Object.keys(this.destinationFilter).includes(destination)) {
+  //       this.destinationFilter[destination] = true;
+  //     }
+  //   }
 
-  getMinDate() {
-    const dates = this.tours?.map((tour: any) => new Date(tour.startDate)) || [];
+  //   for (let destination of Object.keys(this.destinationFilter)) {
+  //     if (!available.includes(destination)) {
+  //       this.destinationFilter[destination] = true;
+  //     }
+  //   }
 
-    if (dates.length === 0) return '';
+  //   return available;
+  // }
 
-    const minDate = new Date(Math.min(...dates));
+  // getAvailableRatings() {
 
-    return minDate.toISOString().split('T')[0];
-  }
+  //   let available = this.tours?.map((tour: any) => String(tour.rating)) || [];
 
-  getMaxDate() {
-    const dates = this.tours?.map((tour: any) => new Date(tour.endDate)) || [];
+  //   available = available.filter((rating: any, index: any) => {
+  //     return available.indexOf(rating) === index;
+  //   })
 
-    if (dates.length === 0) return '';
+  //   for (let rating of available) {
+  //     if (!Object.keys(this.ratingsFilter).includes(rating.toString())) {
+  //       this.ratingsFilter[rating] = true;
+  //     }
+  //   }
 
-    const maxDate = new Date(Math.max(...dates));
+  //   return available;
+  // }
 
-    return maxDate.toISOString().split('T')[0];
-  }
+  // getMinDate() {
+  //   const dates = this.tours?.map((tour: any) => new Date(tour.startDate)) || [];
+
+  //   if (dates.length === 0) return '';
+
+  //   const minDate = new Date(Math.min(...dates));
+
+  //   return minDate.toISOString().split('T')[0];
+  // }
+
+  // getMaxDate() {
+  //   const dates = this.tours?.map((tour: any) => new Date(tour.endDate)) || [];
+
+  //   if (dates.length === 0) return '';
+
+  //   const maxDate = new Date(Math.max(...dates));
+
+  //   return maxDate.toISOString().split('T')[0];
+  // }
 
 
-  getTourPriceIndication(tour: any) {
-    const mostExpensive = Math.max(...this.tours.map((t: any) => t.price));
-    const cheapest = Math.min(...this.tours.map((t: any) => t.price));
+  getTourPriceIndication(tour: Tour) {
+
+    if (this.getTours().length < 2) return {};
+
+    const mostExpensive = Math.max(...this.getTours().map((t: any) => t.price));
+    const cheapest = Math.min(...this.getTours().map((t: any) => t.price));
 
     if (tour.price === mostExpensive) {
       return { 'border': '2px solid red' }; // Style for most expensive
@@ -134,49 +112,49 @@ export class ToursComponent {
     }
   }
 
-  onCurrencyChange(newCurrencyCode: any) {
-    this.currencyCode = newCurrencyCode;
-  }
+  // onCurrencyChange(newCurrencyCode: any) {
+  //   this.currencyCode = newCurrencyCode;
+  // }
 
-  deleteTour(tour: any) {
-    this.tours = this.tours.filter((t: any) => t !== tour);
-  }
+  // deleteTour(tour: any) {
+  //   this.tours = this.tours.filter((t: any) => t !== tour);
+  // }
 
-  bookTour(tour: any) {
-    this.toursBooking[tour.id]++;
-  }
+  // bookTour(tour: any) {
+  //   this.toursBooking[tour.id]++;
+  // }
 
-  cancelTour(tour: any) {
-    this.toursBooking[tour.id]--;
-  }
+  // cancelTour(tour: any) {
+  //   this.toursBooking[tour.id]--;
+  // }
 
-  getNewId() {
-    const ids = this.tours.map((tour: any) => tour.id);
+  // getNewId() {
+  //   const ids = this.tours.map((tour: any) => tour.id);
 
-    return Math.max(...ids) + 1;
-  }
+  //   return Math.max(...ids) + 1;
+  // }
 
-  addTour() {
-    const tour = this.tourForm.value;
+  // addTour() {
+  //   const tour = this.tourForm.value;
 
-    tour['rating'] = 0;
-    tour['id'] = this.getNewId();
-    this.toursBooking[tour.id] = 0;
+  //   tour['rating'] = 0;
+  //   tour['id'] = this.getNewId();
+  //   this.toursBooking[tour.id] = 0;
 
-    this.tours.push(tour);
-    this.tourForm.reset();
-  }
+  //   this.tours.push(tour);
+  //   this.tourForm.reset();
+  // }
 
-  pushToHistory(bookingInfos: any) {
+  // pushToHistory(bookingInfos: any) {
 
-    bookingInfos.forEach((bookingInfo: any) => {
-      bookingInfo['dateBought'] = new Date().toISOString().split('T')[0];
-      this.history.push(bookingInfo);
-    });
+  //   bookingInfos.forEach((bookingInfo: any) => {
+  //     bookingInfo['dateBought'] = new Date().toISOString().split('T')[0];
+  //     this.history.push(bookingInfo);
+  //   });
 
-    bookingInfos.forEach((bookingInfo: any) => {
-      this.toursBooking[bookingInfo.tour.id] = 0;
-      bookingInfo.tour.bookedSeats += bookingInfo.bookedSeats;
-    });
-  }
+  //   bookingInfos.forEach((bookingInfo: any) => {
+  //     this.toursBooking[bookingInfo.tour.id] = 0;
+  //     bookingInfo.tour.bookedSeats += bookingInfo.bookedSeats;
+  //   });
+  // }
 }
