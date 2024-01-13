fc548e7e94901df0fe4a500f120a996629c6bf8f
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2024-01-13 23:41:10 +0100

    Update ratings

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,17 +1,18 @@
-import { DecimalPipe, NgClass } from '@angular/common';
+import { DecimalPipe, NgClass, NgIf } from '@angular/common';
 import { Component, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
   standalone: true,
-  imports: [NgClass, DecimalPipe],
+  imports: [NgClass, DecimalPipe, NgIf],
   templateUrl: './rating.component.html',
   styleUrl: './rating.component.css'
 })
 export class RatingComponent {
   @Input() average: number = 0;
   @Input() current: number = 0;
+  @Input() numberOfRatings: number = 0;
 
   @Output() rated = new EventEmitter<number>();
 
--- src/app/shared/services/tours-rating.service.ts
+++ src/app/shared/services/tours-rating.service.ts
@@ -1,20 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Tour } from '../interfaces/tour';
+import { Rating } from '../interfaces/rating';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToursRatingService {
 
-  toursRating: { [key: number]: number } = {};
+  toursRatings: Rating[] = [];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<any>('assets/tours.json').subscribe(data => {
 
-  getTourRating(tour: Tour) {
-    return this.toursRating[tour.id] || 0;
+      Object.keys(data.ratings).forEach((key: any) => {
+        this.toursRatings.push(
+          {
+            id: parseInt(key),
+            ...data.ratings[key]
+          }
+        );
+      });
+
+    });
+  }
+
+  getAverageTourRating(tour: Tour) {
+    const ratings = this.getRatingsForTour(tour);
+    const sum = ratings.reduce((total, rating) => total + rating.rating, 0);
+    return sum / (ratings.length == 0 ? 1 : ratings.length);
+  }
+
+  getLocalTourRating(tour: Tour) {
+    return this.getRatingsForTour(tour).find((rating: Rating) => rating.userId == 0)?.rating || 0;
+  }
+
+  getNumberOfRatings(tour: Tour) {
+    return this.getRatingsForTour(tour).length;
+  }
+
+  getRatingsForTour(tour: Tour) {
+    return this.toursRatings.filter((rating: Rating) => rating.tourId == tour.id);
   }
 
   rateTour(tour: Tour, rating: number) {
-    this.toursRating[tour.id] = rating;
+
+    const localRating = this.getRatingsForTour(tour).find((rating: Rating) => rating.userId == 0);
+
+    if (localRating) {
+      localRating.rating = rating;
+      return;
+    }
+
+    this.toursRatings.push({
+      id: 100,
+      userId: 0,
+      tourId: tour.id,
+      rating: rating,
+    });
   }
 }
